f493f0b7317e1f325e0575e496e76035593e4944
CobayeGunther <cobayegunter@tuta.io>
2017-10-08 21:17:50 +0200

    Delete botmodules.py

--- botmodules.py
+++
@@ -1,9 +0,0 @@
-# coding=<UTF-8>
-class Module:
-
-    def create(self, name, functions, keyboard):
-        self.name = name
-        self.functions = functions
-        self.keyboard=[['initial', 'keyboard'],
-                     ['please populate', 'your module'],
-                     ['cheers']]
