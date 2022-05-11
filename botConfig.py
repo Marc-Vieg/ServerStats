5d86adf610b69065659b092e2c2b22b040075433
cycolm <cycolm@pc-cycolm.home>
2022-05-12 01:17:33 +0200

    Example of a complete version of telepot with OOP and async. A JSON file defines the menu. Just fill tokens.py informations and remove '_example' to run it.

--- botConfig.py
+++ botConfig.py
@@ -1,77 +1,220 @@
 # -*- coding: utf-8 -*-
+
 from configobj import ConfigObj
-import botglobalvars as MyGlobals
+from datetime import datetime
 import os
-import time
 
 
-def loadSettings(path):
-    #load settings from file to replace default values in MyGlobals (temporary)
-    MyGlobals.sendAlerts = getConfig(path, 'Alerts', 'sendAlerts', 'bool')
-    MyGlobals.autoSend = getConfig(path, 'Alerts', 'autoSend', 'bool')
-    MyGlobals.autoSendTime = getConfig(path, 'Alerts', 'autoSendTime', 'int')
-    MyGlobals.memorythreshold = getConfig(path, 'Graph', 'memth', 'int')
-    MyGlobals.usagethreshold = getConfig(path, 'Graph', 'cputh', 'int')
-    MyGlobals.GraphicHours = getConfig(path, 'Graph', 'length', 'int')
-    MyGlobals.poll = getConfig(path, 'Bot', 'poll', 'int')
-    print("settings loaded", MyGlobals.sendAlerts)
-
-
-def setConfig(path, section, option, value):
-    config = ConfigObj(path)
-    config[section][option] = value
-    print(section, option, value, config[section][option])
-    try:
-        config.write()
-    except:
-        print("error writing config file")
-
-
-def exist(path):
-    return os.path.exists(path)
-
-
-def createConfig(path):
+class BotConfig:
     """
-    Create a config file
+    This is an object to define bot configuration
     """
-    config = ConfigObj(path)
-    graphSection = {
-        'length': 12,
-        'cputh': 90,
-        'memth': 90
-    }
-    config['Graph'] = graphSection
+    def __init__(self, path):
+        # Bot informations
+        self.path = path
 
-    alertSection = {
-        'sendAlerts': 1,
-        'autoSend': 0,
-        'autoSendTime': 30
-    }
-    config['Alerts'] = alertSection
-    botSection = {
-        'poll': 10
-    }
-    config['Bot'] = botSection
+        if not self.exist(self.path):
+            self.createConfig()
+            self.setConfig('Graph', 'length', '60')
 
-    config.write()
+        self.sendAlerts = False
+        self.autoSend = False
+        self.autoSendTime = 30
+        self.memorythreshold = 60  # If memory usage more this %
+        self.usagethreshold = 70  # If cpu usage is more than this %
+        self.poll = 25200  # seconds (7h)
+        self.LISTSMAX = 864000
+        # Hours to show in grap (can be 0.5 to 30 minutes for example)
+        self.GraphicHours = 3 * 3600
+        self.MyIp = '0'
+        self.xaxis = []
+        self.xaxiscpu = []
+        self.xaxistemp = []
+        self.settingmemth = []
+        self.settingcputh = []
+        self.setpolling = []
+        self.graphstart = datetime.now()
+        self.shellexecution = []
+        self.myCores = ['Core 0', 'Core 1']
 
+        # Keyboard
+        self.currentMenu = 'Main'
 
-def getConfig(path, section, option, type):
-    config = ConfigObj(path)
-    if type == 'str':
-        return config[section][option]
-    elif type == 'int':
-        return config[section].as_int(option)
-    elif type == 'bool':
-        #print('bool : ', section, option, config[section].as_bool(option))
-        result = config[section].as_bool(option)
-        if result:
-            return 1
+        # Modules
+        self.isEmbyPresent = False
+        self.isPiholePresent = False
+        self.otherMenu = False
+        self.myWebSiteMenu = True
+        self.webSiteURL = ""
+
+        # Database
+        self.Datas = {
+            'timing': [],
+            'cpu': [],
+            'mem': [],
+            'temp': []
+        }
+
+        # Get sensitive data
+        self.TOKEN = ""
+        self.adminchatid = []
+        self.pihole_passwd = ""
+
+        if self.exist(os.path.dirname(os.path.realpath(__file__))
+                      + os.sep
+                      + 'tokens.py'):
+            import tokens
+            self.TOKEN = tokens.telegrambot
+            self.adminchatid = tokens.adminchatid
+            self.pihole_passwd = tokens.pihole_passwd
+
+    def loadSettings(self):
+        # load settings from file to replace default values in MyGlobals
+        isRead, sendAlerts = self._getConfig('Alerts', 'sendAlerts', 'bool')
+        self.sendAlerts = sendAlerts if isRead else False
+
+        isRead, autoSend = self._getConfig('Alerts', 'autoSend', 'bool')
+        self.autoSend = autoSend if isRead else False
+
+        isRead, autoSendTime = self._getConfig('Alerts', 'autoSendTime', 'int')
+        self.autoSendTime = autoSendTime if isRead else 30
+
+        isRead, memorythreshold = self._getConfig('Graph', 'memth', 'int')
+        self.memorythreshold = memorythreshold if isRead else 60
+
+        isRead, usagethreshold = self._getConfig('Graph', 'cputh', 'int')
+        self.usagethreshold = usagethreshold if isRead else 70
+
+        isRead, GraphicHours = self._getConfig('Graph', 'length', 'int')
+        self.GraphicHours = GraphicHours if isRead else 3 * 3600
+
+        isRead, poll = self._getConfig('Bot', 'poll', 'int')
+        self.poll = poll if isRead else 10
+
+        isRead, isEmbyPresent = self._getConfig('Bot', 'isEmbyPresent', 'bool')
+        self.isEmbyPresent = isEmbyPresent if isRead else False
+
+        isRead, isPiholePresent = self._getConfig('Bot',
+                                                  'isPiholePresent',
+                                                  'bool')
+        self.isPiholePresent = isPiholePresent if isRead else False
+
+        isRead, otherMenu = self._getConfig('Bot', 'otherMenu', 'bool')
+        self.otherMenu = otherMenu if isRead else False
+
+        isRead, myWebSiteMenu = self._getConfig('Bot', 'myWebSiteMenu', 'bool')
+        self.myWebSiteMenu = myWebSiteMenu if isRead else False
+
+        isRead, webSiteURL = self._getConfig('Bot', 'webSiteURL', 'str')
+        self.webSiteURL = webSiteURL if isRead else False
+
+    def _getConfig(self, section, option, type):
+        try:
+            config = ConfigObj(self.path)
+            if type == 'str':
+                return True, config[section][option]
+            elif type == 'int':
+                return True, config[section].as_int(option)
+            elif type == 'bool':
+                return True, config[section].as_bool(option)
+        except Exception:
+            return False, None
+
+    def setConfig(self, section, option, value):
+        config = ConfigObj(self.path)
+        config[section][option] = value
+
+        if section == 'Graph':
+            if option == 'length':
+                self.GraphicHours = value
+            elif option == 'cputh':
+                self.usagethreshold = value
+            elif option == 'memth':
+                self.memorythreshold = value
+        elif section == 'Alerts':
+            if option == 'sendAlerts':
+                self.sendAlerts = value
+            elif option == 'autoSendTime':
+                self.autoSendTime = value
+            elif option == 'autoSend':
+                self.autoSend = value
+        elif section == 'Bot':
+            if option == 'poll':
+                self.poll = value
+            elif option == 'isEmbyPresent':
+                self.isEmbyPresent = value
+            elif option == 'isPiholePresent':
+                self.isPiholePresent = value
+            elif option == 'otherMenu':
+                self.otherMenu = value
+            elif option == 'myWebSite':
+                self.myWebSiteMenu = value
+            elif option == 'webSiteURL':
+                self.webSiteURL = value
+
+        try:
+            config.write()
+        except Exception:
+            print("Something went wrong while updating the settings file")
+
+    def exist(self, path):
+        return os.path.exists(path)
+
+    def createConfig(self):
+        """
+        Create a config file
+        """
+        config = ConfigObj(self.path)
+
+        config['Graph'] = {
+            'length': 12,
+            'cputh': 90,
+            'memth': 90
+        }
+        config['Alerts'] = {
+            'sendAlerts': 1,
+            'autoSend': 0,
+            'autoSendTime': 30
+        }
+        config['Bot'] = {
+            'poll': 10,
+            'isEmbyPresent': 0,
+            'isPiholePresent': 0,
+            'otherMenu': 0,
+            'myWebSiteMenu': 0,
+            'webSiteURL': ""
+        }
+
+        try:
+            config.write()
+        except Exception:
+            print("Something went wrong while creating the settings file")
+
+    def getKeyboard(self, itemsList=[], addBackTouch=True):
+        """
+        This function gives you a specific keyboard
+        """
+        def chunks(lst, n):
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
+
+        keyboard = []
+        if itemsList:
+            if addBackTouch and "<- back" not in itemsList:
+                itemsList.append("<- back")
+            for line in chunks(itemsList, 3):
+                keyboard.append(line)
         else:
-            return 0
+            keyboard = []
+        return keyboard
 
-if __name__ == "__main__":
-    path = "settings.ini"
-    createConfig(path)
-    setConfig(path, 'graph', 'length', '60')
+    def isAuthorizedId(self, chat_id):
+        return chat_id in self.adminchatid
+
+    def clearall(self, chat_id):
+        if chat_id in self.shellexecution:
+            self.shellexecution.remove(chat_id)
+        if chat_id in self.settingmemth:
+            self.settingmemth.remove(chat_id)
+        if chat_id in self.setpolling:
+            self.setpolling.remove(chat_id)
--- servstatsbot.py
+++ servstatsbot.py
@@ -1,15 +1,54 @@
 # -*- coding: utf-8 -*-
-import botfunction as funk
+
+import asyncio
+from multiprocessing import Process
 import os
-import botConfig as config
+import telepot
+from telepot.aio.loop import MessageLoop
+from telepot.aio.delegate import pave_event_space, per_chat_id, create_open
+from botTools import BotTools
+# bot tools
+from botData import BotData
+from botConfig import BotConfig
+# Message handler
+from mainMessageHandler import MainMessageHandler
+from routine import Routine
+
+TIMEOUT = 20 * 60
 
 
 if __name__ == "__main__":
+    # Get proc id
     pid = os.getpid()
-    op = open("ssb.pid", "w")
-    op.write("%s" % pid)
+    op = open(os.path.dirname(os.path.abspath(__file__)) + "/ssb.pid", "w")
+    op.write(str(pid))
     op.close()
-    if not config.exist('settings.ini'):
-        config.createConfig('settings.ini')
-    config.loadSettings('settings.ini')
-    funk.main()
+
+    # Init config
+    config = BotConfig(os.path.dirname(os.path.abspath(__file__))
+                       + "/settings.ini")
+    config.loadSettings()
+    data = BotData()
+    tools = BotTools(config, data)
+
+    # Run Bot
+    bot = telepot.aio.DelegatorBot(config.TOKEN, [
+        pave_event_space()(
+            per_chat_id(), create_open,
+            MainMessageHandler, config, data, tools,
+            timeout=TIMEOUT
+        )
+    ])
+
+    monitoring = Routine(config, data, tools)
+    process = Process(
+        target=monitoring.routine,
+        daemon=True
+    )
+    process.start()
+
+    # Message Handler
+    loop = asyncio.get_event_loop()
+    loop.create_task(MessageLoop(bot).run_forever())
+    print('Listening ...')
+    loop.run_forever()
